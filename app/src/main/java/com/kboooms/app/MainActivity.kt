7321f5f7db314e3e0b8feae105cc68d31bc0a1d1
cardonajuansiuu7-gif <cardonajuansiuu7@gmail.com>
2026-08-11 16:30:19 -0500

    Update MainActivity.kt

--- app/src/main/java/com/kboooms/app/MainActivity.kt
+++ app/src/main/java/com/kboooms/app/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
